3846d80a8d03c561d855199fb13217b0615a9670
Stefan Eugster <stefan.eugster@llv.li>
2014-11-28 16:45:44 +0100

    bugfix: set initial editor content

--- widget.js
+++ widget.js
@@ -1,43 +1,55 @@
 WAF.define('CKEditor', ['waf-core/widget'], function(widget) {
 	
 	var _this,
-	    ckEditorDatasource;
+	    ckEditorDatasource,
+	    ckEditorContent;
 	
     var CKEditor = widget.create('CKEditor', {
         init: function() {
-        	
-        	_this = this;
+            try {
+            	_this = this;
 
-        	// create editor with CKEditor plugin
-        	_this.editor = CKEDITOR.appendTo(_this.node, {
-                customConfig: _this.customConfigPath()
-            });
+            	// create editor with CKEditor plugin
+            	_this.editor = CKEDITOR.appendTo(_this.node, {
+                    customConfig: _this.customConfigPath()
+                });
 
-            // add content to datasource
-            _this.editor.on('change', function( e ) {
-            	// clear timer
-            	if ( _this.throttle ) {
-            		clearTimeout(_this.throttle);
-            	}
-            	// throttle function calls to every 250 milliseconds
-            	_this.throttle = setTimeout(function(){
-                    // set datasource value
-                    _this.content(_this.editor.getData());
-            	}, 250);
-            });
-            // set editor contetn on datasource current element change
-            ckEditorDatasource = _this.content.boundDatasource();
-            
-            WAF.sources[ckEditorDatasource.datasourceName].addListener("onCurrentElementChange", function (event){
-                _this.editor.setData(event.dataSource[ckEditorDatasource.attribute]);
-            });
+                // add content to datasource
+                _this.editor.on('change', function( e ) {
+                	// clear timer
+                	if ( _this.throttle ) {
+                		clearTimeout(_this.throttle);
+                	}
+                	// throttle function calls to every 250 milliseconds
+                	_this.throttle = setTimeout(function(){
+                        // set datasource value
+                        _this.content(_this.editor.getData());
+                	}, 250);
+                });
+                
+                // set editor content on datasource current element change
+                ckEditorDatasource = _this.content.boundDatasource();
+                
+                // set editor content on current element change
+                WAF.sources[ckEditorDatasource.datasourceName].addListener("onCurrentElementChange", function (event){
+                    ckEditorContent = event.dataSource[ckEditorDatasource.attribute];
+
+                    if (event.eventKind == 'onCurrentElementChange' && ckEditorContent) {
+                        _this.setValue(ckEditorContent);
+                    }
+                });
+                
+                // set initial editor content
+                _this.editor.on('instanceReady', function(){
+                    _this.setValue(ckEditorContent);
+                });	
+            } catch (e) {
+                console.log(e.message);
+            }
         },
         content: widget.property({
             type: 'string',
-    		defaultValue: '',
-    		onChange: function(value) {
-//    			_this.editor.setData(value);
-            }
+    		defaultValue: ''
     	}),
     	customConfigPath: widget.property({
     		type: 'string',
